49b0468b977a6ae0b0ab8e7284d38c494b6cbd03
MOIYS <leajaehuck3@gmail.com>
2025-10-17 22:47:36 +0900

    feat: 쉼표와 콜론 구분자 동시 지원

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -4,8 +4,8 @@ public class StringCalculator {
 
     public int calculate(String text) {
 
-        // 쉼표(,)를 기준으로 문자열을 분리하여, 숫자를 저장하는 문자열 배열을 만듦
-        String[] numbers = text.split(",");
+        // 쉼표(,)와 콜론(:)을 기준으로 문자열을 분리하여, 숫자를 저장하는 문자열 배열을 만듦
+        String[] numbers = text.split("[,:]");
 
         int sum = 0;
 
