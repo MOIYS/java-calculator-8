8706b3365ec6cc78da4701bb3660f8e16bbaf07f
MOIYS <leajaehuck3@gmail.com>
2025-10-17 22:33:09 +0900

    feat: 쉼표 구분자 합산 기능

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,9 +2,15 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
+
         Input input = new Input();
+        StringCalculator stringCalculator = new StringCalculator();
 
         String userInput = input.requestInput();
         System.out.println("사용자가 입력한 문자열: " + userInput);
+
+        int result = stringCalculator.calculate(userInput);
+
+        System.out.println("결과: " + result);
     }
 }
