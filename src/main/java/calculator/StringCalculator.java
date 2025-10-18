ea7e9b3bd2e3768e436fa9ee86a04d00bc08d00b
MOIYS <leajaehuck3@gmail.com>
2025-10-18 21:07:40 +0900

    feat: 커스텀 및 기본 구분자 동시 지원

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,11 +1,28 @@
 package calculator;
 
+import java.util.regex.Pattern;  // Pattern.quote
+
 public class StringCalculator {
+
+    // 기본 구분자로 쉼표(,)와 콜론(:) 사용
+    private static final String DEFAULT_DELIMITERS_REGEX = "[,:]";
 
     public int calculate(String text) {
 
-        // 쉼표(,)와 콜론(:)을 기준으로 문자열을 분리하여, 숫자를 저장하는 문자열 배열을 만듦
-        String[] numbers = text.split("[,:]");
+        String finalRegex = DEFAULT_DELIMITERS_REGEX;
+
+        // 입력이 "//"로 시작하면, 커스텀 구분자 형식으로 간주
+        if(text.startsWith("//")) {
+            String[] parts = text.split(Pattern.quote("\\n"), 2);
+            String header = parts[0];
+            String body = parts[1];
+
+            text = body;
+            String customDelimiter = header.substring(2);
+            finalRegex = Pattern.quote(customDelimiter) + "|" + DEFAULT_DELIMITERS_REGEX;
+        }
+
+        String[] numbers = text.split(finalRegex);
 
         int sum = 0;
 
