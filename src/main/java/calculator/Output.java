988d43768196382bbde058a9996f39df439f8564
MOIYS <leajaehuck3@gmail.com>
2025-10-18 21:33:18 +0900

    refactor: 출력 기능 분리

---
+++ src/main/java/calculator/Output.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class Output {
+
+    public void printInputConfirmation(String userInput) {
+        System.out.println("사용자가 입력한 문자열: " + userInput);
+    }
+
+    public void printResult(int result) {
+        System.out.println("결과: " + result);
+    }
+}
